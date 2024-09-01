81b55d2130b247dc453bf515ffe1f8019f1cfffc
Matias Badano <115195236+MatiasJB95@users.noreply.github.com>
2024-09-01 18:48:59 -0300

    Create Comentarios.java

---
+++ BackEnd/src/main/java/C20_48_t_Python_React/demo/persistence/entity/Comentarios.java
@@ -0,0 +1,33 @@
+package C20_48_t_Python_React.demo.persistence.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "comentarios")
+public class Comentarios {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String contenido;
+
+    @ManyToOne
+    @JoinColumn(name = "usuario_id", nullable = false)
+    private Usuarios usuarios;
+
+    @ManyToOne
+    @JoinColumn(name = "receta_id", nullable = false)
+    private Recetas recetas;
+
+    @Column(name = "fecha_comentario", nullable = false)
+    private LocalDateTime fechaComentario;
+};
