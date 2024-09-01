2c3ba604ae55ddb8e3397a2cf7f1cb0b67f83852
Matias Badano <115195236+MatiasJB95@users.noreply.github.com>
2024-09-01 18:48:55 -0300

    Create Categorias.java

---
+++ BackEnd/src/main/java/C20_48_t_Python_React/demo/persistence/entity/Categorias.java
@@ -0,0 +1,20 @@
+package C20_48_t_Python_React.demo.persistence.entity;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "categorias")
+public class Categorias {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+
+    @OneToMany(mappedBy = "categorias")
+    private List<RecetaCategoria> recetaCategorias;
+}
