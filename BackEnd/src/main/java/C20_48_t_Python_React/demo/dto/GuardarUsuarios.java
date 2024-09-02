5338e4534f7d2e5026108cce91195c1afcbf2bbc
Matias Badano <115195236+MatiasJB95@users.noreply.github.com>
2024-09-02 20:33:01 -0300

    Create GuardarUsuarios.java

---
+++ BackEnd/src/main/java/C20_48_t_Python_React/demo/dto/GuardarUsuarios.java
@@ -0,0 +1,31 @@
+package C20_48_t_Python_React.demo.dto;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class GuardarUsuarios implements Serializable {
+    @Size(min=4)
+    private String nombre;
+    @Size(min=4)
+    private String email;
+    @Size(min=8)
+    private String contrasena;
+    @Size(min=8)
+    private String repeatcontrasena;
+
+
+    public String getRepeatcontrasena() {
+        return repeatcontrasena;
+    }
+    public void setRepeatPassword(String repeatcontrasena) {
+        this.repeatcontrasena = repeatcontrasena;
+    }
+
+}
