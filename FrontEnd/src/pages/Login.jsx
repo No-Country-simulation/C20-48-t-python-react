e861c3f43b0011088eed363978524effb3c74129
Adolfo Sánchez López <sanchezlopezadolfo@gmail.com>
2024-09-04 18:41:21 -0500

    Create Login and Register

--- FrontEnd/src/pages/Login.jsx
+++ FrontEnd/src/pages/Login.jsx
@@ -1,12 +1,18 @@
 import { Link } from "react-router-dom"
-
+import { TextField } from "@mui/material" 
+import { Button } from "@mui/material"
+import "./Pages.css";
 function Login() {
-  return (
-    <>
-      <Link to="/">Volver</Link>
-      <p>Login</p>
-    </>
-  )
+return (<>
+
+    <div className="loginRegisterCard">
+    <div><h2>Iniciar sesión</h2></div>
+    <div><TextField id="standard-basic" label="Email" variant="standard" /></div>
+    <div><TextField id="standard-basic" label="Contraseña" variant="standard" /></div>
+    <div><Button variant="contained">Iniciar sesión</Button></div>
+    </div>
+
+</>)
 }
 
 export default Login
--- FrontEnd/src/pages/Register.jsx
+++ FrontEnd/src/pages/Register.jsx
@@ -1,12 +1,18 @@
 import { Link } from "react-router-dom";
-
+import { TextField } from "@mui/material" 
+import { Button } from "@mui/material"
+import "./Pages.css";
 function Register() {
-  return (
-    <>
-      <Link to="/">Volver</Link>
-      <p>pagina de registro</p>
-    </>
-  )
+return (<>
+
+    <div className="loginRegisterCard">
+    <div><h2>Registrate</h2></div>
+    <div><TextField id="standard-basic" label="Email" variant="standard" /></div>
+    <div><TextField id="standard-basic" label="Contraseña" variant="standard" /></div>
+    <div><Button variant="contained">Registrate</Button></div>
+    </div>
+
+</>)
 }
 
 export default Register;
