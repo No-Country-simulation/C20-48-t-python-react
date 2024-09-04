6586a7512094ea17f06eb32138907a5c6261d245
Matias Badano <115195236+MatiasJB95@users.noreply.github.com>
2024-09-03 22:44:33 -0300

    Update Valoraciones.java

--- BackEnd/src/main/java/C20_48_t_Python_React/demo/persistence/entity/Valoraciones.java
+++ BackEnd/src/main/java/C20_48_t_Python_React/demo/persistence/entity/Valoraciones.java
@@ -1,4 +1,17 @@
 package C20_48_t_Python_React.demo.persistence.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "valoraciones")
 public class Valoraciones {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+
 }
