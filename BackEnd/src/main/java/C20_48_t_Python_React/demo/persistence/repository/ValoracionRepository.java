27b88a7c2611adceaecc46a38988ec2aacd1c904
Matias Badano <115195236+MatiasJB95@users.noreply.github.com>
2024-09-03 23:03:22 -0300

    Update ValoracionRepository.java

--- BackEnd/src/main/java/C20_48_t_Python_React/demo/persistence/repository/ValoracionRepository.java
+++ BackEnd/src/main/java/C20_48_t_Python_React/demo/persistence/repository/ValoracionRepository.java
@@ -1,5 +1,11 @@
 package C20_48_t_Python_React.demo.persistence.repository;
 
-public interface ValoracionRepository
-{
+import C20_48_t_Python_React.demo.persistence.entity.Valoraciones;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface ValoracionRepository extends JpaRepository<Valoraciones, Long> {
+    @Query("SELECT AVG(v.puntuacion) FROM Valoracion v WHERE v.receta.id = :recetaId")
+    Double findAveragePuntuacionByRecetaId(@Param("recetaId") Long recetaId);
 }
