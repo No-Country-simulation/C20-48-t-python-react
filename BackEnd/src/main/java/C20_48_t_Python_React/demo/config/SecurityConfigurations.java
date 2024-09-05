5f6caa80dc5dc911b9cac9569919f85d1ffcc325
Matias Badano <115195236+MatiasJB95@users.noreply.github.com>
2024-09-05 16:22:41 -0300

    Update SecurityConfigurations.java

--- BackEnd/src/main/java/C20_48_t_Python_React/demo/config/SecurityConfigurations.java
+++ BackEnd/src/main/java/C20_48_t_Python_React/demo/config/SecurityConfigurations.java
@@ -1,5 +1,90 @@
 package C20_48_t_Python_React.demo.config;
 
+import C20_48_t_Python_React.demo.exeption.ObjectNotFoundException;
+import C20_48_t_Python_React.demo.persistence.repository.UsuariosRepository;
+import C20_48_t_Python_React.demo.service.impl.UsuariosDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfigurations {
 
+    @Autowired
+    private UsuariosDetailsService userDetailService;
+    @Autowired
+    private UsuariosRepository usuariosRepository;
+
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailService).passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    @Order(0)
+    SecurityFilterChain publicSecurityFilterChain(HttpSecurity http) throws Exception {
+     return http
+             .securityMatcher("/home", "/login")
+             .authorizeHttpRequests((authorize -> authorize.anyRequest().permitAll()))
+             .build();
+}
+@Bean
+    @Order(1)
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(csrf -> csrf.disable())  //
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
+                .authorizeHttpRequests(http -> {
+                    // Configurar los endpoints públicos primero
+                    http.requestMatchers(HttpMethod.GET, "/auth/get").permitAll();
+                    http.requestMatchers(HttpMethod.GET, "/home").permitAll();
+                    http.requestMatchers(HttpMethod.POST, "/register").permitAll();
+                    http.requestMatchers(HttpMethod.GET, "/user").hasRole("USER");
+
+
+                    http.anyRequest().authenticated();
+                })
+
+                .build();
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailService);
+        provider.setPasswordEncoder(passwordEncoder());
+        return provider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
