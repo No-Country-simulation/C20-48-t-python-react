fb57d81ae0aaf6442f73e0f850be7322a43876ae
Matias Badano <115195236+MatiasJB95@users.noreply.github.com>
2024-09-05 16:24:10 -0300

    Controllers

---
+++ BackEnd/src/main/java/C20_48_t_Python_React/demo/controller/CreateAcountController.java
@@ -0,0 +1,23 @@
+package C20_48_t_Python_React.demo.controller;
+
+import C20_48_t_Python_React.demo.dto.GuardarUsuarios;
+import C20_48_t_Python_React.demo.dto.RegisteredUser;
+import C20_48_t_Python_React.demo.service.AuthenticationService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/register")
+public class CreateAcountController {
+    @Autowired
+    private AuthenticationService authenticationService;
+    @PostMapping()
+    public ResponseEntity<RegisteredUser> registerOne(@RequestBody @Valid GuardarUsuarios newUser) {
+        RegisteredUser registeredUser = (RegisteredUser) authenticationService.registerOneCustomer(newUser);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(registeredUser);
+    }
+}
