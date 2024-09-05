e248551fd3fd85dc4ffcd869b59150e861bff7ce
Matias Badano <115195236+MatiasJB95@users.noreply.github.com>
2024-09-05 16:25:05 -0300

    Repositorys

--- BackEnd/src/main/java/C20_48_t_Python_React/demo/persistence/repository/UsuariosRepository.java
+++ BackEnd/src/main/java/C20_48_t_Python_React/demo/persistence/repository/UsuariosRepository.java
@@ -4,7 +4,10 @@ import C20_48_t_Python_React.demo.persistence.entity.Usuarios;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UsuariosRepository extends JpaRepository<Usuarios, Long> {
 
+    Optional<Usuarios> findByEmail(String email);
 }
--- BackEnd/src/main/java/C20_48_t_Python_React/demo/persistence/repository/ValoracionRepository.java
+++ BackEnd/src/main/java/C20_48_t_Python_React/demo/persistence/repository/ValoracionRepository.java
@@ -4,8 +4,9 @@ import C20_48_t_Python_React.demo.persistence.entity.Valoraciones;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ValoracionRepository extends JpaRepository<Valoraciones, Long> {
-    @Query("SELECT AVG(v.puntuacion) FROM Valoracion v WHERE v.receta.id = :recetaId")
-    Double findAveragePuntuacionByRecetaId(@Param("recetaId") Long recetaId);
+
 }
