e8883fa3893a45c6ac4ba0d92901e7361bf2f3c1
JuanManuelSanjurjo <juanmanuelsanjurjo@gmail.com>
2024-09-06 16:41:25 -0300

    Renderizacion condicional del Speed Dial para crear recetas en el home y editar en el detalle de la misma

--- FrontEnd/src/components/FloatingAB.jsx
+++ FrontEnd/src/components/FloatingAB.jsx
@@ -1,11 +1,34 @@
 import Box from "@mui/material/Box";
-import Fab from "@mui/material/Fab";
+import SpeedDial from "@mui/material/SpeedDial";
+import SpeedDialIcon from "@mui/material/SpeedDialIcon";
+import SpeedDialAction from "@mui/material/SpeedDialAction";
+import EditIcon from "@mui/icons-material/Edit";
 import AddIcon from "@mui/icons-material/Add";
+import { useState } from "react";
+import { useLocation } from "react-router-dom";
+
+const actionsHome = [{ icon: <AddIcon />, name: "Crear" }];
+
+const actionsDetalleReceta = [{ icon: <EditIcon />, name: "Editar" }];
 
 export default function FloatingAB() {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  // Dependiendo de la ruta, selecciona las acciones a mostrar
+  // TODO: que el speedDial solo se muestre si la receta es del usuario!!!!!
+
+  const location = useLocation();
+  const actions =
+    location.pathname === "/" ? actionsHome : actionsDetalleReceta;
+
   return (
     <Box
       sx={{
+        height: 330,
+        transform: "translateZ(0px)",
+        flexGrow: 1,
         m: 1,
         position: "fixed",
         bottom: 16,
@@ -15,9 +38,24 @@ export default function FloatingAB() {
         zIndex: 1,
       }}
     >
-      <Fab color="primary" aria-label="add">
-        <AddIcon />
-      </Fab>
+      <SpeedDial
+        ariaLabel="SpeedDial tooltip example"
+        sx={{ position: "absolute", bottom: 16, right: 16 }}
+        icon={<SpeedDialIcon />}
+        onClose={handleClose}
+        onOpen={handleOpen}
+        open={open}
+      >
+        {actions.map((action) => (
+          <SpeedDialAction
+            key={action.name}
+            icon={action.icon}
+            tooltipTitle={action.name}
+            tooltipOpen
+            onClick={handleClose}
+          />
+        ))}
+      </SpeedDial>
     </Box>
   );
 }
