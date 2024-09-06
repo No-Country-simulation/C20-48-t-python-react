1dad286ca42af9f9bd790e455bc01b8ce546d499
JuanManuelSanjurjo <juanmanuelsanjurjo@gmail.com>
2024-09-06 15:52:03 -0300

    Functionality in SearchBar.jsx component, interacting with AutoCompleteSelection.jsx

--- FrontEnd/src/components/SearchBar.jsx
+++ FrontEnd/src/components/SearchBar.jsx
@@ -11,8 +11,32 @@ import { useTheme, useMediaQuery } from "@mui/material";
 import { useState, useRef } from "react";
 
 export default function SearchBar() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [category, setCategory] = useState("");
+  const [ingredient, setIngredient] = useState("");
+  const [difficulty, setDifficulty] = useState("");
   const [open, setOpen] = useState(false);
   const ref = useRef();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log({ searchTerm, category, ingredient, difficulty });
+  };
+  const handleSearch = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const handleCategory = (value) => {
+    setCategory(value);
+  };
+
+  const handleIngredient = (value) => {
+    setIngredient(value);
+  };
+
+  const handleDifficulty = (value) => {
+    setDifficulty(value);
+  };
 
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down("lg"));
@@ -24,75 +48,92 @@ export default function SearchBar() {
     }
   }
   return (
-    <Container
-      maxWidth="xl"
-      disableGutters
-      sx={{
-        paddingBlock: 2,
-        display: "grid",
-        gap: 2,
-        gridTemplateColumns: {
-          xs: "1fr",
-          lg: "2fr 3fr 1fr",
-        },
-        justifyItems: "start",
-      }}
-    >
+    <form onSubmit={handleSubmit}>
       <Container
+        maxWidth="xl"
         disableGutters
-        sx={{ display: "flex", flexDirection: "row", gap: 2 }}
-      >
-        <TextField
-          sx={{
-            width: "100%",
-          }}
-          placeholder="Buscar recetas..."
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">
-                <SearchIcon />
-              </InputAdornment>
-            ),
-          }}
-        />
-        <IconButton onClick={handleFilterToggle}>
-          <FilterIcon />
-        </IconButton>
-      </Container>
-      <Container
-        disableGutters
-        ref={ref}
-        sx={{ display: "none", width: "100%" }}
-      >
-        <Collapse in={open} orientation={isMobile ? "vertical" : "horizontal"}>
-          <Container
-            disableGutters
-            sx={{
-              display: "grid",
-              gap: 2,
-              gridTemplateColumns: {
-                xs: "1fr",
-                lg: "repeat(3, minmax(170px, 1fr))",
-              },
-            }}
-          >
-            <AutoCompleteSelection field="Categoria" />
-            <AutoCompleteSelection field="Ingrediente" />
-            <AutoCompleteSelection field="Dificultad" />
-          </Container>
-        </Collapse>
-      </Container>
-      <Button
-        variant="contained"
         sx={{
-          width: {
-            xs: "100%",
-            lg: "max-content",
+          paddingBlock: 2,
+          display: "grid",
+          gap: 2,
+          gridTemplateColumns: {
+            xs: "1fr",
+            lg: "2fr 3fr 1fr",
           },
+          justifyItems: "start",
         }}
       >
-        Buscar
-      </Button>
-    </Container>
+        <Container
+          disableGutters
+          sx={{ display: "flex", flexDirection: "row", gap: 2 }}
+        >
+          <TextField
+            sx={{
+              width: "100%",
+            }}
+            placeholder="Buscar recetas..."
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <SearchIcon />
+                </InputAdornment>
+              ),
+            }}
+            onChange={handleSearch}
+          />
+          <IconButton onClick={handleFilterToggle}>
+            <FilterIcon />
+          </IconButton>
+        </Container>
+        <Container
+          disableGutters
+          ref={ref}
+          sx={{ display: "none", width: "100%" }}
+        >
+          <Collapse
+            in={open}
+            orientation={isMobile ? "vertical" : "horizontal"}
+          >
+            <Container
+              disableGutters
+              sx={{
+                display: "grid",
+                gap: 2,
+                gridTemplateColumns: {
+                  xs: "1fr",
+                  lg: "repeat(3, minmax(170px, 1fr))",
+                },
+              }}
+            >
+              <AutoCompleteSelection
+                field="Categoria"
+                handler={handleCategory}
+              />
+              <AutoCompleteSelection
+                field="Ingrediente"
+                handler={handleIngredient}
+              />
+              <AutoCompleteSelection
+                field="Dificultad"
+                handler={handleDifficulty}
+              />
+            </Container>
+          </Collapse>
+        </Container>
+        <Button
+          variant="contained"
+          size="large"
+          type="submit"
+          sx={{
+            width: {
+              xs: "100%",
+              lg: "max-content",
+            },
+          }}
+        >
+          Buscar
+        </Button>
+      </Container>
+    </form>
   );
 }
