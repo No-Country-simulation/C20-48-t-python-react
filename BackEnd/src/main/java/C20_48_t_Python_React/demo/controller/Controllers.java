4c2d997a075b1703a49f0ba28eb9209d44044483
Matias Badano <115195236+MatiasJB95@users.noreply.github.com>
2024-09-05 23:07:35 -0300

    controllers

---
+++ BackEnd/src/main/java/C20_48_t_Python_React/demo/controller/Controllers.java
@@ -0,0 +1,23 @@
+package C20_48_t_Python_React.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controllers {
+
+    @GetMapping("/req/login")
+    public String login(){
+        return "login";
+    }
+
+    @GetMapping("/req/signup")
+    public String signup(){
+        return "signup";
+    }
+    @GetMapping("/index")
+    public String home(){
+        return "index";
+    }
+}
---
+++ BackEnd/src/main/java/C20_48_t_Python_React/demo/controller/LoginController.java
@@ -0,0 +1,15 @@
+package C20_48_t_Python_React.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/login")
+public class LoginController {
+    @GetMapping()
+   public String login() {
+        return "Hellow word login";
+    }
+}
