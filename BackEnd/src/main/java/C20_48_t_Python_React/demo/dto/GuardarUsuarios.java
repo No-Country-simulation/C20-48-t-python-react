f20514ceffd6c0c81a8c81cb23ee542d7536de46
Matias Badano <115195236+MatiasJB95@users.noreply.github.com>
2024-09-06 22:30:36 -0300

    Update

--- BackEnd/src/main/java/C20_48_t_Python_React/demo/dto/GuardarUsuarios.java
+++ BackEnd/src/main/java/C20_48_t_Python_React/demo/dto/GuardarUsuarios.java
@@ -1,4 +1,6 @@
 package C20_48_t_Python_React.demo.dto;
+import C20_48_t_Python_React.demo.persistence.entity.Recetas;
+import C20_48_t_Python_React.demo.persistence.entity.Likes;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -7,6 +9,7 @@ import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
@@ -22,6 +25,18 @@ public class GuardarUsuarios implements Serializable {
     private String contrasena;
     @Size(min=8)
     private String repeatcontrasena;
+    private String Rol;
+    private int statusCode;
+    private String error;
+    private String message;
+    private String token;
+    private String refreshToken;
+    private String expirationTime;
+    private List<Recetas> listRecetas;
+    private List<Likes> listLikes;
+
+
+
 
 
     public String getRepeatcontrasena() {
