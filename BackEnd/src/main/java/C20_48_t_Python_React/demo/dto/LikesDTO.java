b085900520683505df461c9d352771038f5b5215
Matias Badano <115195236+MatiasJB95@users.noreply.github.com>
2024-09-09 17:26:05 -0300

    DTOs

---
+++ BackEnd/src/main/java/C20_48_t_Python_React/demo/dto/LikesDTO.java
@@ -0,0 +1,15 @@
+package C20_48_t_Python_React.demo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class LikesDTO {
+    private Long id;
+    private Long usuarioId;
+}
