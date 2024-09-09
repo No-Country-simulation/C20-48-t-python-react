22534e1876f7017c254567ef0bcf807c118205e3
Matias Badano <115195236+MatiasJB95@users.noreply.github.com>
2024-09-09 17:26:17 -0300

    Repositorys

--- BackEnd/src/main/java/C20_48_t_Python_React/demo/persistence/repository/RecetasRepository.java
+++ BackEnd/src/main/java/C20_48_t_Python_React/demo/persistence/repository/RecetasRepository.java
@@ -4,6 +4,9 @@ import C20_48_t_Python_React.demo.persistence.entity.Recetas;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface RecetasRepository extends JpaRepository<Recetas, Long> {
+    List<Recetas> findByUsuariosId(Long usuarioId);
 }
