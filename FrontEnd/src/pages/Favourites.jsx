92e634f21881469987a835117a452b1ff6dc2187
Isa696 <isaias92@hotmail.com.ar>
2024-09-12 18:05:05 -0300

    Feat MyRecipes.jsx - add context and filter id & id_usuario to display on screen

--- FrontEnd/src/pages/Favourites.jsx
+++ FrontEnd/src/pages/Favourites.jsx
@@ -1,5 +1,5 @@
 import DisplayCategories from "../components/DisplayCategories";
-import Container from "@mui/material/Container";
+// import Container from "@mui/material/Container";
 import { recetas } from "../assets/recetas";
 export default function Favourites() {
   return <DisplayCategories recetas={recetas} category={"Favourites"} />;
--- FrontEnd/src/pages/MyRecipes.jsx
+++ FrontEnd/src/pages/MyRecipes.jsx
@@ -1,5 +1,16 @@
 import DisplayCategories from "../components/DisplayCategories";
-import { recetas } from "../assets/recetas";
+import { useContext } from "react";
+import { RecipeListContext } from "../Context/RecipeContext";
+import { UserContext } from "../Context/UserContext";
 export default function Favourites() {
-  return <DisplayCategories recetas={recetas} category={"My Recipes"} />;
+  const { recipes } = useContext(RecipeListContext);
+  const {userInfo} = useContext(UserContext)
+
+  const misRecetas = recipes.filter((receta) => receta.id_usuario === userInfo.id);
+
+  return(
+    <>
+    <DisplayCategories recetas={misRecetas} category={"Mis Recetas"} />;
+    </>
+  )
 }
