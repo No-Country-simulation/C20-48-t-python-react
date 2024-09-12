f821aeca50724e834a824c23694086bddef38a22
Matias Badano <115195236+MatiasJB95@users.noreply.github.com>
2024-09-12 15:28:19 -0300

    EditarReceta

--- BackEnd/src/main/java/C20_48_t_Python_React/demo/persistence/repository/PasosRepository.java
+++ BackEnd/src/main/java/C20_48_t_Python_React/demo/persistence/repository/PasosRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface PasosRepository extends JpaRepository<Pasos, Long> {
 
+    void deleteByRecetasId(Long recetaId);
 }
--- BackEnd/src/main/java/C20_48_t_Python_React/demo/persistence/repository/RecetaCategoriaRepository.java
+++ BackEnd/src/main/java/C20_48_t_Python_React/demo/persistence/repository/RecetaCategoriaRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface RecetaCategoriaRepository extends JpaRepository<RecetaCategoria, Long> {
 
+    void deleteByRecetasId(Long recetaId);
 }
