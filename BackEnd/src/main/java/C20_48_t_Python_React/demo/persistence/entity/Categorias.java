023abb0f0e5923b9746c674fffb45a55005fb93f
Matias Badano <115195236+MatiasJB95@users.noreply.github.com>
2024-09-14 20:52:49 -0300

    deploy

--- BackEnd/src/main/java/C20_48_t_Python_React/demo/persistence/entity/Categorias.java
+++ BackEnd/src/main/java/C20_48_t_Python_React/demo/persistence/entity/Categorias.java
@@ -13,6 +13,7 @@ public class Categorias {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(length = 100)
     private String nombre;
 
     @OneToMany(mappedBy = "categorias")
