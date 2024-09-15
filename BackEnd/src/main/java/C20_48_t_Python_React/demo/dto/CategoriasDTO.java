42ffabb482422be06f57bd6723e7f760e88e0abb
Matias Badano <115195236+MatiasJB95@users.noreply.github.com>
2024-09-14 22:47:26 -0300

    Create CategoriasDTO.java

---
+++ BackEnd/src/main/java/C20_48_t_Python_React/demo/dto/CategoriasDTO.java
@@ -0,0 +1,16 @@
+package C20_48_t_Python_React.demo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CategoriasDTO {
+    private Long id;
+    private String nombre;
+
+}
