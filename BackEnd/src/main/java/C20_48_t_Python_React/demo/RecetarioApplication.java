57e30ba6d81c01089c18197b0058f14d3c66d9cf
Juan Manuel Sanjurjo <57844658+JuanManuelSanjurjo@users.noreply.github.com>
2024-10-03 14:24:49 -0300

    Update RecetarioApplication.java

--- BackEnd/src/main/java/C20_48_t_Python_React/demo/RecetarioApplication.java
+++ BackEnd/src/main/java/C20_48_t_Python_React/demo/RecetarioApplication.java
@@ -2,8 +2,10 @@ package C20_48_t_Python_React.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class RecetarioApplication {
 
 	public static void main(String[] args) {
