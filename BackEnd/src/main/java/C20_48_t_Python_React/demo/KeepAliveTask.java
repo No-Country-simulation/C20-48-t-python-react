4a0219d5046a471a4ff62b34a0d86cf5a37d8244
Juan Manuel Sanjurjo <57844658+JuanManuelSanjurjo@users.noreply.github.com>
2024-10-03 15:58:44 -0300

    Update KeepAliveTask.java

--- BackEnd/src/main/java/C20_48_t_Python_React/demo/KeepAliveTask.java
+++ BackEnd/src/main/java/C20_48_t_Python_React/demo/KeepAliveTask.java
@@ -1,3 +1,5 @@
+package C20_48_t_Python_React.demo;
+
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
